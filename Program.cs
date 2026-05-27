ed24ee4b3d3254dd4bcb672c93212cdccc5c8e26
Neyser <RR23072@ues.edu.sv>
2026-05-26 21:18:45 -0600

    titulo del programa

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace proyecto_GT13GP06
+{
+	class Program
+	{
+		static void Main(string[] args)
+        {
+            Console.WriteLine("Librería y Papelería Universitaria UES");
+            Console.WriteLine("");
+        }
+	}
+}
